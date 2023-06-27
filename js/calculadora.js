f4f97078d914a2eb98a53829aad405de4604b3cf
HunigoleSan <soy.hugo.pe@gmail.com>
2023-06-26 23:35:45 -0500

    Cargando funciones JS calculadora

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,15 +1,73 @@
-/*
-  Plantilla: HunigoleSan
-*/
-/*        PROBLEMA: Programa tu calculadora */
-/* 
-   Deberás cumplir todas las operaciones básicas: suma, resta, multiplicación, división,
-   también debe permitir limpiar el resultado.
+const btn = document.querySelectorAll("button");
+const display = document.querySelector("#igual");
+const result = document.querySelector("#resultado")
+const limpiarHtml = document.querySelector("#limpiar")
+const recuperarDato = false
+const resetear = false
+var datos = "";
 
-   PROPÓSITO: esta tarea consiste en lograr conseguir el objetivo para desarrollar la lógica de programación
-   MISIÓN: Aplicar todos los conocimientos que tienes sobre JavaScript para lograr el objetivo
-   VISIÓN: Comprometete a terminar la tarea, para confirmar tu propia determinación, hacia tus metas.
+btn.forEach((btn)=>{
+  btn.addEventListener("click", ()=>{
 
-   NOTA: Dependerá de ti, que tan lejos desees llevar la funcionalidad de la calculadora
-   PSDT: Si crees que es necesario cambiar la estructura del HTML y CSS, puedes hacerlo con gusto.
-   */
+    let data = [1,2,6,1,2,5,9,'33','33'];
+    
+    let result = data.filter((item,index)=>{
+      return data.indexOf(item) === index;
+    })
+    console.log(result);
+
+    result.innerHTML += btn.textContent;
+    datos = result.textContent
+    datos.filter
+    /* if (datos.includes("/")){
+      console.log("entro al if")
+      datos = datos.replace("/","/")
+      console.log(datos)
+
+    } */
+    console.log(datos)
+    
+    console.log(btn.textContent);
+
+
+  });
+});
+
+function reemplazarOperador(){
+
+}
+
+
+limpiarHtml.addEventListener('click', ()=>{
+  resetearC(datos)
+})
+display.addEventListener("click", function() {
+  let operacion = validarMultiplicacion(datos)
+  
+  if (operacion.includes("=")){
+
+    datosReplace = operacion.replace("=","")
+    console.log(datosReplace)
+    result.innerHTML = eval(datosReplace)
+    /* recuperarDato = true */
+  }
+  
+  
+});
+
+function validarMultiplicacion(operacion){
+  if (datos.includes("x")){
+    datosReplace = datos.replace("x","*")
+    console.log(datosReplace)
+    return datosReplace
+  }
+  return operacion
+}
+
+function resetearC(datos){
+  result.innerHTML = "";
+  
+}
+
+
+
