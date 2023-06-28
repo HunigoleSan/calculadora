a2c236b95191ad7abc071bec1d4eef3d7d2f4217
HunigoleSan <soy.hugo.pe@gmail.com>
2023-06-28 18:54:07 -0500

    subiendo actualizaciones (falta validar las cadenas)

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,73 +1,104 @@
 const btn = document.querySelectorAll("button");
 const display = document.querySelector("#igual");
 const result = document.querySelector("#resultado")
+const mensajeHtml = document.querySelector("#mensaje")
 const limpiarHtml = document.querySelector("#limpiar")
 const recuperarDato = false
 const resetear = false
 var datos = "";
 
-btn.forEach((btn)=>{
-  btn.addEventListener("click", ()=>{
-
-    let data = [1,2,6,1,2,5,9,'33','33'];
-    
-    let result = data.filter((item,index)=>{
-      return data.indexOf(item) === index;
-    })
-    console.log(result);
+btn.forEach((btn) => {
+  btn.addEventListener("click", () => {
 
     result.innerHTML += btn.textContent;
     datos = result.textContent
-    datos.filter
-    /* if (datos.includes("/")){
-      console.log("entro al if")
-      datos = datos.replace("/","/")
-      console.log(datos)
+    firtCaracter = datos.charAt(0)
+    /*Esto falta mejorar*/
+    validationFirstCaracterOperator(firtCaracter)
 
-    } */
-    console.log(datos)
     
-    console.log(btn.textContent);
 
 
   });
 });
+function ValidationMultiplicityOfOperatorContinuous(cadena){
+  let newChain = ""
+  let ultimoCaracter =""
+  for (let i = 0; i < cadena.length; i++) {
+      let caracter = cadena.charAt(i);
 
-function reemplazarOperador(){
+      console.log("Linea 30:",caracter)
+      if(caracter !== '/' && caracter !=='x' && caracter !=='+'&& caracter !=='-' && caracter !=='.'){
+        console.log("Linea 32:",caracter)
+        newChain += caracter
+      }else{
+          console.log("Linea 35:",caracter)
+          /* ultimoCaracter = cadena.charAt(i+1) */
+          if(caracter !== cadena.charAt(i+1)){
+            if(caracter == '/' ||caracter == '+' || caracter == '-' || caracter === '.' || caracter === 'x'){
+              console.log("No agrega: ", caracter)
+          
+            }else{
+                newChain += caracter
+            }
+          }else{
+              console.log("raro")
+              /* newChain += caracter */
+          }
+      }
+    }
+    return newChain
+  }
+
+let cadena = "aa++///--bb"
+let cadenaModificada = ValidationMultiplicityOfOperatorContinuous(cadena)
+console.log("Cadena normal: ",cadena)
+console.log("Cadena modificada: ", cadenaModificada)
+
+
+function validationFirstCaracterOperator(firtCaracter){
+  if (firtCaracter === '/' ||
+  firtCaracter === 'x' ||
+  firtCaracter === '-' ||
+  firtCaracter === '+' ||
+  firtCaracter === '.') {
+  console.log("Entra en primer error")
+  firtsValidationError = true
+  mensajeHtml.innerHTML = 'Por favor debe iniciar con un operador'
 
 }
 
-
-limpiarHtml.addEventListener('click', ()=>{
+}
+limpiarHtml.addEventListener('click', () => {
   resetearC(datos)
 })
-display.addEventListener("click", function() {
-  let operacion = validarMultiplicacion(datos)
-  
-  if (operacion.includes("=")){
+function resetearC(datos) {
+  result.innerHTML = "";
 
-    datosReplace = operacion.replace("=","")
+}
+
+display.addEventListener("click", function () {
+  let operacion = validarMultiplicacion(datos)
+
+  if (operacion.includes("=")) {
+
+    datosReplace = operacion.replace("=", "")
     console.log(datosReplace)
     result.innerHTML = eval(datosReplace)
     /* recuperarDato = true */
   }
-  
-  
+
 });
 
-function validarMultiplicacion(operacion){
-  if (datos.includes("x")){
-    datosReplace = datos.replace("x","*")
+function validarMultiplicacion(operacion) {
+  if (datos.includes("x")) {
+    datosReplace = datos.replace("x", "*")
     console.log(datosReplace)
     return datosReplace
   }
   return operacion
 }
 
-function resetearC(datos){
-  result.innerHTML = "";
-  
-}
 
 
 
